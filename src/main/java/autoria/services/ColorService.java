bb4a5d2e78f846b92a08fdce90223f3a0006b25f
Anatolii.Zamlynnyi <Anatolii.Zamlynnyi@gmail.com>
2020-07-25 16:10:24 +0300

    Edit

--- src/main/java/autoria/services/ColorService.java
+++ src/main/java/autoria/services/ColorService.java
@@ -3,9 +3,6 @@ package autoria.services;
 import autoria.entities.Color;
 import autoria.repositories.ColorRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface ColorService {
     Color findByName(String name);
 
--- src/main/java/autoria/web/ColorController.java
+++ src/main/java/autoria/web/ColorController.java
@@ -2,31 +2,18 @@ package autoria.web;
 
 import autoria.entities.Color;
 import autoria.repositories.ColorRepository;
-import autoria.services.ColorService;
 import autoria.services.ColorServiseImpl;
-import org.omg.CORBA.UserException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.lang.model.type.ErrorType;
-import java.util.List;
 import java.util.Optional;
-
 
 @Controller
 public class ColorController {
-    //private final ColorService service;
-
-//    public ColorController(ColorService service) {
-//        this.service = service;
-//    }
 
     @Autowired
     private ColorRepository colorRepository;
@@ -36,14 +23,6 @@ public class ColorController {
     @GetMapping("/color")
     public String index(Model model)
     {
-//        List<Color> colors = colorRepository.findAll();
-//        try {
-//            Optional<Color> col = colorRepository.findById(3);
-//            Color cols = col.get();
-//        }
-//        catch (Exception ex){
-//            return null;
-//        }
         model.addAttribute("colors", colorRepository.findAll());
         return "color";
     }
@@ -68,7 +47,6 @@ public class ColorController {
         catch (Exception ex){
             return null;
         }
-        //colorService.save(color);
         return "deletecolor";
     }
 
