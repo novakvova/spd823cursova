8253866ffb546e3b2ddfea3f1d99efbeaac47719
Volodymyr Novak <novakvova@gmail.com>
2020-08-29 17:21:21 +0300

    Add image and video gallery

--- src/main/java/autoria/web/GalleryController.java
+++ src/main/java/autoria/web/GalleryController.java
@@ -1,10 +1,13 @@
 package autoria.web;
 
+import autoria.entities.Color;
+import autoria.entities.Gallery;
 import autoria.repositories.GalleryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,4 +22,17 @@ public class GalleryController
         model.addAttribute("list", galleryRepository.findAll());
         return "gallery/index";
     }
+
+    @GetMapping("/create")
+    public String create() {
+        //colorService.save(color);
+        return "gallery/create";
+    }
+
+    @PostMapping("/create")
+    public String create(Gallery color){
+        galleryRepository.save(color);
+        return "redirect:/gallery";
+    }
+
 }
