ffdcd72fbbd572a71cb6e887877cee75d89515ce
Volodymyr Novak <novakvova@gmail.com>
2020-09-12 14:23:14 +0300

    Add upload file option

--- src/main/java/autoria/SPD823Application.java
+++ src/main/java/autoria/SPD823Application.java
@@ -1,8 +1,11 @@
 package autoria;
 
+import autoria.services.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SPD823Application extends SpringBootServletInitializer
@@ -11,5 +14,11 @@ public class SPD823Application extends SpringBootServletInitializer
 	{
 		SpringApplication.run(SPD823Application.class, args);
 	}
-
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			//storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
